d6d2cc9eb5bf5571bcb9d04efa9eec0498050025
Jepthan <jefte.jh.jh@gmail.com>
2023-04-15 03:21:31 -0600

    navbar function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
 
-function App() {
-  
+
+function Navbar(){
 
   return (
+  <nav>
+    <ul>
+      <li>1 element</li>
+      <li>2 element</li>
+      <li>3 element</li>
+    </ul>
+  </nav>
+  )
+}
+export default function App() {
+  return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   )
 }
 
-export default App
