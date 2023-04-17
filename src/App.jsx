8d84cf6f406f0cee88f74398764ab9099142c3e8
Jepthan <jefte.jh.jh@gmail.com>
2023-04-17 00:03:51 -0600

    agregar tailwind y react three fiber

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import './App.css'
+import { Canvas, useFrame } from '@react-three/fiber'
 
-
-function Navbar(){
+function Navbar() {
 
   return (
-  <nav>
-    <ul>
-      <li>1 element</li>
-      <li>2 element</li>
-      <li>3 element</li>
-    </ul>
-  </nav>
+    <nav>
+      <ul>
+        <li>1 element</li>
+        <li>2 element</li>
+        <li>3 element</li>
+      </ul>
+    </nav>
   )
 }
+
 export default function App() {
+
+  const ref = useRef()
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <h1 className='text-3xl font-bold underline'>
+        Hello word
+      </h1>
+
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh
+          ref={ref}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={'orange'} />
+        </mesh>
+      </Canvas>
     </div>
   )
 }
