e86096c9312de6bab656ff322cf71261fe4852e7
Jepthan <jefte.jh.jh@gmail.com>
2023-07-20 22:36:16 -0600

    add some skills and change font-family

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,14 +4,14 @@ export default function About(props) {
     const { t } = useTranslation()
 
     return (
-        <div className=' text-white' name={props.name}>
-            <h1 className="text-center text-3xl font-bold m-6">{t('about_header')}</h1>
-            <div className="text-center m-auto max-w-3xl">
+        <div className=' text-white w-11/12 m-auto' name={props.name}>
+            <h1 className="text-center text-3xl font-extrabold mb-6">{t('about_header')}</h1>
+            <div className="text-center m-auto max-w-7xl ">
                 {t("about_body")}
             </div>
             <div className="flex flex-wrap mt-14">
                 <div className="mr-6 ml-6 w-96 grow">
-                    <h1 className="text-center font-bold text-xl mb-4">Get to know me</h1>
+                    <h1 className="text-center font-extrabold text-xl mb-4">Get to know me</h1>
                     <p className="text-lg mb-3">
                         {t("about_content1p1")}
                     </p>
@@ -51,6 +51,12 @@ export default function About(props) {
                         </div>
                         <div className="rounded bg-secondary px-4 py-2 mr-3 mb-3">
                             Tailwind
+                        </div>
+                        <div className="rounded bg-secondary px-4 py-2 mr-3 mb-3">
+                            Python
+                        </div>
+                        <div className="rounded bg-secondary px-4 py-2 mr-3 mb-3">
+                            ASP.NET
                         </div>
                     </div>
                 </div>
